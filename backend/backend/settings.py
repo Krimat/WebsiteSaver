40eaea7f5cbcbfc2727f521038c810904236e2b9
Arthur <arthuromgnh@gmail.com>
2023-02-02 09:46:48 +0200

    changed static files dirs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'frontend/static'
+STATIC_ROOT = BASE_DIR.parent / 'frontend/build/static'
 STATICFILES_DIRS = [
-    BASE_DIR.parent / 'frontend/build/static',
     BASE_DIR.parent / 'frontend/build',
     ]
 
