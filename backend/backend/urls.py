7f627508ad2b4e4711482987ef84dabd6e805d0f
Arthur <arthuromgnh@gmail.com>
2023-02-08 14:38:56 +0200

    added debug staticfiles view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 from .views import index
@@ -24,3 +25,5 @@ urlpatterns = [
     path('api/', include('websiteSaver.urls')),
     path('', index),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
