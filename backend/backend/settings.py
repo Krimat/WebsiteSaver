909fbaf3beb30e69984fcccd2639a670eac805b0
Arthur <arthuromgnh@gmail.com>
2023-02-08 14:17:24 +0200

    Changed media dir to work after collectstatic

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 #Image handling
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = STATIC_ROOT / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
