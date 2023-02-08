51a45fef6f55ad1b9084d07df3fc05f374e34478
Arthur <arthuromgnh@gmail.com>
2023-02-08 14:18:06 +0200

    Added str function to models Tags no longer necessary for site

--- backend/websiteSaver/models.py
+++ backend/websiteSaver/models.py
@@ -11,13 +11,19 @@ class Tag(models.Model):
             models.UniqueConstraint(fields=['name'], name='name_constraint'),
         ]
 
+    def __str__(self):
+        return f'#{self.name}'
+
 class Site(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=1024)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     added = models.DateField(auto_now=True)
     url = models.CharField(max_length=512)
-    tags = models.ManyToManyField(Tag, related_name='sites')
+    tags = models.ManyToManyField(Tag, related_name='sites', blank=True)
+
+    def __str__(self):
+        return f"{self.name}  url:{self.url}"
 
 
     
