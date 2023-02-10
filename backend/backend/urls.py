800655f82517ceaf43bffa11be9b3e22deeec699
Arthur <arthuromgnh@gmail.com>
2023-02-10 11:58:13 +0200

    urlpatterns fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-url_patterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
